b8c561be8b37e4640678cceea784af8dab479b35
Vedant Jha <6vedant@gmail.com>
2020-02-12 13:21:09 +0530

    Toolar title added

--- app/src/main/java/xyz/vedant/liveweather/Splash.java
+++ app/src/main/java/xyz/vedant/liveweather/Splash.java
@@ -1,8 +1,15 @@
 package xyz.vedant.liveweather;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class Splash extends AppCompatActivity {
 
@@ -10,11 +17,38 @@ public class Splash extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        try {
+            getSupportActionBar().hide();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
         loadData();
     }
 
     public void loadData() {
+
+        FirebaseDatabase.getInstance().getReference().child("live_weather")
+                .addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                        String live_api_url = (String)dataSnapshot.child("live_api").getValue();
+                        String forecast_api_url = (String)dataSnapshot.child("forecasting_api").getValue();
+
+                        Intent intent = new Intent(Splash.this, TemperatureActivity.class);
+                        intent.putExtra("live_api", live_api_url);
+                        intent.putExtra("forecast_api", forecast_api_url);
+                        startActivity(intent);
+                        finish();
+
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
 
     }
 
--- app/src/main/java/xyz/vedant/liveweather/TemperatureActivity.java
+++ app/src/main/java/xyz/vedant/liveweather/TemperatureActivity.java
@@ -4,19 +4,31 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
 import xyz.vedant.liveweather.data.ForecastData;
 
-public class ScrollingActivity extends AppCompatActivity {
+public class TemperatureActivity extends AppCompatActivity {
 
     String curr_temp;
+    String curr_city;
+
+    public String getCurr_city() {
+        return curr_city;
+    }
+
+    public void setCurr_city(String curr_city) {
+        this.curr_city = curr_city;
+    }
 
     public String getCurr_temp() {
         return curr_temp;
@@ -35,6 +47,11 @@ public class ScrollingActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         setCurr_temp("0");
+        setCurr_city("Bengaluru");
+
+        final String live_api = getIntent().getStringExtra("live_api");
+        final String forecast_api = getIntent().getStringExtra("forecast_api");
+
 
         try {
             // getSupportActionBar().setTitle(getTitle());
@@ -46,7 +63,7 @@ public class ScrollingActivity extends AppCompatActivity {
 
 
         try {
-            ForecastData forecastData = new ForecastData(ScrollingActivity.this, recyclerView);
+            ForecastData forecastData = new ForecastData(TemperatureActivity.this, recyclerView);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -56,11 +73,38 @@ public class ScrollingActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 setCurr_temp("" + System.currentTimeMillis());
+
+                Toast.makeText(getApplicationContext(), "" + live_api + " " + forecast_api, Toast.LENGTH_SHORT).show();
                 setToolbarText("26" + getString(R.string.degree_str));
                 Snackbar.make(view, "Replace with your own action" + getCurr_temp(), Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
+
+
+        // set the title of toolbar
+        // when scrolled up
+        final CollapsingToolbarLayout collapsingToolbarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        AppBarLayout appBarLayout = (AppBarLayout) findViewById(R.id.app_bar);
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = true;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    collapsingToolbarLayout.setTitle("" + getCurr_temp() + getString(R.string.degree_str) + " - " + getCurr_city());
+                    isShow = true;
+                } else if (isShow) {
+                    collapsingToolbarLayout.setTitle(" ");//careful there should a space between double quote otherwise it wont work
+                    isShow = false;
+                }
+            }
+        });
+
     }
 
     @Override
